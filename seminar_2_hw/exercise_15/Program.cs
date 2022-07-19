b3168aaa87d9281d1c11dfad95314703efe8a8fb
Kilya265 <liliya.nikodimova@yandex.ru>
2022-07-19 23:21:20 +0700

    Задача 15

---
+++ seminar_2_hw/exercise_15/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число: ");
+int day = Convert.ToInt32(Console.ReadLine());
+int res;
+
+if (day > 7) res = day % 7;
+else res = day;
+
+Console.WriteLine(res);
+
+Console.WriteLine("Этот день выходной?");
+if (res == 0 || res > 5) Console.WriteLine("да");
+else Console.WriteLine("нет");
