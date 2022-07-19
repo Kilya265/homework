5ba55111e07c85bd925afe5988c86f6cd4bc0dd1
Kilya265 <liliya.nikodimova@yandex.ru>
2022-07-19 23:19:35 +0700

    Задача 10

---
+++ seminar_2_hw/exercise_10/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+
+if (number.Length > 2) Console.WriteLine(number[1]);
+else Console.WriteLine("Введено не трёхзначное число");
