49a468561b595c72ce33dcd1a10e0f447d74296f
Kilya265 <liliya.nikodimova@yandex.ru>
2022-07-19 23:20:38 +0700

    Задача 13

---
+++ seminar_2_hw/exercise_13/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+
+if (number.Length > 2) Console.WriteLine(number[2]);
+else Console.WriteLine("Третьей цифры нет");
