b48fa5547f8c5efea1b5f17dc2cbd30904e74176
Kilya265 <liliya.nikodimova@yandex.ru>
2022-07-22 02:17:31 +0700

    Задача 19

---
+++ seminar_3_hw/exercise_19/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число:");
+
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+
+void CheckingNumber(string stringNumber){
+  if (stringNumber[0]==stringNumber[4] || stringNumber[1]==stringNumber[3]){
+    Console.WriteLine($"Число: {stringNumber} - палиндром.");
+  }
+  else Console.WriteLine($"Число: {stringNumber} - НЕ палиндром.");
+}
+
+if (stringNumber.Length == 5){
+  CheckingNumber(stringNumber);
+}
+else Console.WriteLine("Введено не пятизначное число!");
