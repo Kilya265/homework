23f7045ac92d1364171a3668968926cf0cc32aeb
Kilya265 <liliya.nikodimova@yandex.ru>
2022-07-22 15:57:38 +0700

    Задача 18

---
+++ seminar_3_hw/exercise_18/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите номер Четверти: ");
+
+int n = Int32.Parse(Console.ReadLine());
+
+if (n == 1) {
+    Console.WriteLine("x[0;∞) и y[0;∞)");
+} else if (n == 2) {
+    Console.WriteLine("x(-∞;0] и y[0;∞)");
+} else if (n == 3) {
+    Console.WriteLine("x(-∞;0] и y(-∞;0]");
+} else if (n == 4) {
+    Console.WriteLine("x[0;∞) и y(-∞;0]");
+} else {
+    Console.WriteLine("Неправильный ввод!");
+}
