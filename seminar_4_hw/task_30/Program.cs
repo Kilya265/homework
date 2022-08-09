df1b48f87b47ea420a808ee8130245dae0ee7fa8
Kilya265 <liliya.nikodimova@yandex.ru>
2022-08-09 16:39:44 +0700

    Task 30

---
+++ seminar_4_hw/task_30/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var r = new Random();
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++){
+    array[i] = r.Next(0,2);
+}
+Console.Write("Массив: [");
+foreach (int i in array)
+Console.Write($"{i},");
+Console.WriteLine("]");
