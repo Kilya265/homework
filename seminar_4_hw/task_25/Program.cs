af5e23f4959e7c6af7460c009bb462ec7a52930c
Kilya265 <liliya.nikodimova@yandex.ru>
2022-08-09 12:11:55 +0700

    Task 25

---
+++ seminar_4_hw/task_25/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int IntPow(int a, int b) {
+  int result = 1;
+  for(int i=1; i <= b; i++) {
+    result *= a;
+  }
+    return result;
+}
+Console.WriteLine("Цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.");
+Console.Write("A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int intPow = IntPow(a, b);
+Console.WriteLine("Ответ: " + intPow);
