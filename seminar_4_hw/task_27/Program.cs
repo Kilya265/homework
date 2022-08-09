f1a3b71b70ebd3536f497ba521a3ebb0c7e3feb1
Kilya265 <liliya.nikodimova@yandex.ru>
2022-08-09 14:09:55 +0700

    Task 27

---
+++ seminar_4_hw/task_27/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+foreach (char a in x.ToString())
+sum += Convert.ToInt32(a.ToString());
+Console.WriteLine("Сумма цифр: " + sum);
