69fe3f6205f8ff258ecf086e040ec00f248df1b4
Kilya265 <liliya.nikodimova@yandex.ru>
2022-08-09 12:59:57 +0700

    Task 26

---
+++ seminar_4_hw/task_26/Program.cs
@@ -0,0 +1,6 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int count = (int)Math.Log10(x) + 1;
+Console.WriteLine("Кол-во цифр в числе: " + count);
