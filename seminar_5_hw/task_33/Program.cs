fe757c55990db889e07b13da518370b2769a36c3
Kilya265 <liliya.nikodimova@yandex.ru>
2022-08-10 13:33:49 +0700

    Task 33

---
+++ seminar_5_hw/task_33/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте массив. Напишите программу, которая определяет, 
+//присутствует ли заданное число в массиве.
+
+Console.WriteLine("Введите число, которое необходимо найти: ");
+int count = 0;
+int [] array = new int[5];
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < array.Length; i++) {
+    array[i] = new Random().Next(0, 20);
+    Console.Write(array[i] + " ");
+}
+
+foreach (var item in array) {
+    if (item == num) count = 1;
+}
+
+if (count > 0) Console.WriteLine("-> да");
+else Console.WriteLine("-> нет");
