5c8d869b520eb5590066dd0e95e8bee9ee0d9f28
Kilya265 <liliya.nikodimova@yandex.ru>
2022-08-11 15:14:37 +0700

    Task 44

---
+++ seminar_6_hw/task_44/Program.cs
@@ -0,0 +1,19 @@
+﻿//Не используя рекурсию, выведите первые N чисел Фибоначчи.
+//Первые два числа Фибоначчи: 0 и 1.
+
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int firstNumber = 0;
+int secondNumber = 1;
+int result = 0;
+Console.Write($"{firstNumber}, {secondNumber}");
+
+for (int i = 2; i < n; i++) {
+    result = firstNumber + secondNumber;
+    firstNumber = secondNumber;
+    secondNumber = result;
+    Console.Write($", {result}");
+}
+Console.WriteLine();
