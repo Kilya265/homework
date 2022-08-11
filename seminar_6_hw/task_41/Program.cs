8a3fdc19e20c9b6b5dfe9998bf6766c95417eedf
Kilya265 <liliya.nikodimova@yandex.ru>
2022-08-11 16:09:47 +0700

    Task 41

---
+++ seminar_6_hw/task_41/Program.cs
@@ -0,0 +1,14 @@
+﻿//Пользователь вводит с клавиатуры M чисел.
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите числа через пробел: ");
+int[] n = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+int array(int[] n) {
+    int count = 0;
+    foreach (var element in n) {
+        if (element > 0) count++;
+    }
+    return count;
+}
+Console.WriteLine(array(n));
