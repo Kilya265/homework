14fd4968159b4350bf426d8d127a20654a1e8d57
Kilya265 <liliya.nikodimova@yandex.ru>
2022-08-11 15:33:00 +0700

    Task 45

---
+++ seminar_6_hw/task_45/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая будет создавать копию
+//заданного массива с помощью поэлементного копирования.
+
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[n];
+int[] arrayCopy = new int[n];
+
+Console.WriteLine("Массив: ");
+for (int i = 0; i < n; i++) {
+    array[i] = new Random().Next(-10, 100);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+Console.WriteLine("Копия заданного массива: ");
+for (int j = 0; j < n; j++) {
+    arrayCopy[j] = array[j];
+    Console.Write(arrayCopy[j] + " ");
+}
+Console.WriteLine();
