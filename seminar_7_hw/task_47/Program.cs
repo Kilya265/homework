b682a9caa78eb4080effcc6310fc1daa178027c4
Kilya265 <liliya.nikodimova@yandex.ru>
2022-08-19 17:14:45 +0700

    Task 47

---
+++ seminar_7_hw/task_47/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.Write("Введите кол-во строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[m, n];
+
+CreateArrayDouble(array);
+WriteArray(array);
+Console.WriteLine();
+
+void CreateArrayDouble(double[,] array) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            array[i, j] = new Random().NextDouble() * 20 - 10;
+        }
+    }
+}
+
+void WriteArray (double[,] array) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            double num = Math.Round(array[i, j], 1);
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+}
