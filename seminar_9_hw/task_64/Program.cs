bc24787932e563208fb33ad883d5ad02b77d954f
Kilya265 <liliya.nikodimova@yandex.ru>
2022-08-23 18:26:08 +0700

    Task 64

---
+++ seminar_9_hw/task_64/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Задайте значения M и N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от M до N
+*/
+
+Console.Write("Введите M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"M = {M}, N = {N}");
+for (int i = M; i <= N; i++) Console.Write(i + " ");
+Console.WriteLine();
